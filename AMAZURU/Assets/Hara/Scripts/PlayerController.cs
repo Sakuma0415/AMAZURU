b661aae9ac26b30fce89f0de4e1b75f5a9519fbf
Kazunori Hara <rize.game.0816@gmail.com>
2020-03-02 19:55:11 +0900

    [Update]テストシーン作成

---
+++ AMAZURU/Assets/Hara/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField, Header("移動速度")] private float playerSpeed = 1.0f;
+
+    /// <summary>
+    /// 移動を管理する
+    /// </summary>
+    private void PlayerMove()
+    {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            // 前進
+        }
+
+        if(Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            // 後退
+        }
+
+        if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            // 左
+        }
+
+        if(Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            // 右
+        }
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMove();
+    }
+}
