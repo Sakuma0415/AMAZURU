aa4cb7b3760057fcd5a63b8b919652c5a988cc32
Kazunori Hara <rize.game.0816@gmail.com>
2020-03-05 03:01:49 +0900

    [Update]プレイヤーの移動処理を改善

--- AMAZURU/Assets/Hara/Scripts/PlayerController.cs
+++ AMAZURU/Assets/Hara/Scripts/PlayerController.cs
@@ -5,79 +5,59 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
-
+    [SerializeField, Tooltip("PlayerのRigidbody")] private Rigidbody rb = null;
     [SerializeField, Header("移動速度")] private float playerSpeed = 1.0f;
 
-    private bool moveFlag = false;
+    private Vector3 lastPos;
+
+    /// <summary>
+    /// 初期化
+    /// </summary>
+    private void PlayerInit()
+    {
+        lastPos = new Vector3(transform.position.x, 0, transform.position.z);
+    }
 
     /// <summary>
     /// 移動を管理する
     /// </summary>
     private void PlayerMove()
     {
+        // 入力値を取得
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
 
-        if((Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) && (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)))
-        {
-            transform.rotation = Quaternion.Euler(new Vector3(transform.rotation.x, 45, transform.rotation.z));
-            moveFlag = true;
-        }
-        else if ((Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) && (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)))
-        {
-            transform.rotation = Quaternion.Euler(new Vector3(transform.rotation.x, 135, transform.rotation.z));
-            moveFlag = true;
-        }
-        else if ((Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) && (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)))
-        {
-            transform.rotation = Quaternion.Euler(new Vector3(transform.rotation.x, -135, transform.rotation.z));
-            moveFlag = true;
-        }
-        else if ((Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) && (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)))
-        {
-            transform.rotation = Quaternion.Euler(new Vector3(transform.rotation.x, -45, transform.rotation.z));
-            moveFlag = true;
-        }
-        else if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
-        {
-            transform.rotation = Quaternion.Euler(new Vector3(transform.rotation.x, 0, transform.rotation.z));
-            moveFlag = true;
-        }
-        else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
-        {
-            transform.rotation = Quaternion.Euler(new Vector3(transform.rotation.x, 180, transform.rotation.z));
-            moveFlag = true;
-        }
-        else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
-        {
-            transform.rotation = Quaternion.Euler(new Vector3(transform.rotation.x, -90, transform.rotation.z));
-            moveFlag = true;
-        }
-        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
-        {
-            transform.rotation = Quaternion.Euler(new Vector3(transform.rotation.x, 90, transform.rotation.z));
-            moveFlag = true;
-        }
-        else
-        {
-            moveFlag = false;
-        }
+        // 移動処理
+        transform.position += new Vector3(moveX, 0, moveZ) * playerSpeed * Time.deltaTime;
 
-        // キーが入力されていたらその方向に前進
-        if (moveFlag)
-        {
-            transform.position += transform.forward * playerSpeed * Time.deltaTime;
-        }
+        // 移動方向を向く処理
+        Vector3 diff = new Vector3(transform.position.x, 0, transform.position.z) - lastPos;
+        lastPos = new Vector3(transform.position.x, 0, transform.position.z);
         
+        if(diff.magnitude > 0.01f)
+        {
+            transform.localRotation = Quaternion.LookRotation(diff);
+        }
     }
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        PlayerInit();
     }
 
     // Update is called once per frame
     void Update()
     {
         PlayerMove();
+    }
+
+    /// <summary>
+    /// Editor上で初期化
+    /// </summary>
+    private void Reset()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
     }
 }
