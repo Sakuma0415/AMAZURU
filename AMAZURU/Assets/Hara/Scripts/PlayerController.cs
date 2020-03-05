cbc64216ee1e042bdbdc40f030930d4d18d4e406
Kazunori Hara <rize.game.0816@gmail.com>
2020-03-06 04:00:08 +0900

    [Update]キャラクターの移動処理を変更、カメラの向きで移動方向が変わるように修正

--- AMAZURU/Assets/Hara/Scripts/PlayerController.cs
+++ AMAZURU/Assets/Hara/Scripts/PlayerController.cs
@@ -8,57 +8,66 @@ public class PlayerController : MonoBehaviour
     [SerializeField, Tooltip("PlayerのRigidbody")] private Rigidbody rb = null;
     [SerializeField, Header("重力の設定")] private Vector3 localGravity;
     [SerializeField, Header("移動速度")] private float playerSpeed = 1.0f;
+    [SerializeField, Header("移動時の起点カメラ")] private Camera playerCamera = null;
+    public Camera PlayerCamera { set { playerCamera = value; } }
+
+    private Vector3 playerVec = Vector3.zero;
+
+    private bool inputX = false;
+    private bool inputZ = false;
 
     /// <summary>
     /// 初期化
     /// </summary>
     private void PlayerInit()
     {
-        
+        if(playerCamera == null) { playerCamera = Camera.main; }
     }
 
     /// <summary>
     /// 移動を管理する
     /// </summary>
-    private void PlayerMove()
+    private void PlayerMoveControl()
     {
         // 入力値を取得
         float moveX = Input.GetAxis("Horizontal");
         float moveZ = Input.GetAxis("Vertical");
-        Vector3 direction = new Vector3(moveX, 0, moveZ);
+
+        inputX = Mathf.Abs(moveX) > 0.1f;
+        inputZ = Mathf.Abs(moveZ) > 0.1f;
+
+        // カメラの向いている方向を取得
+        Vector3 cameraForward = Vector3.Scale(playerCamera.transform.forward, new Vector3(1, 0, 1)).normalized;
 
         // 床が存在するかをRayでチェック
-        Ray rayForward = new Ray(transform.position, Vector3.forward - transform.up);
-        Ray rayBack = new Ray(transform.position, Vector3.back - transform.up);
-        Ray rayRight = new Ray(transform.position, Vector3.right - transform.up);
-        Ray rayLeft = new Ray(transform.position, Vector3.left - transform.up);
+        Ray rayForward = new Ray(transform.position, cameraForward - transform.up);
+        Ray rayBack = new Ray(transform.position, -cameraForward - transform.up);
+        Ray rayRight = new Ray(transform.position, playerCamera.transform.right - transform.up);
+        Ray rayLeft = new Ray(transform.position, -playerCamera.transform.right - transform.up);
         float rayLength = 10.0f;    // Rayの長さ
         bool findGroundForward = Physics.Raycast(rayForward, out _, rayLength);
         bool findGroundBack = Physics.Raycast(rayBack, out _, rayLength);
         bool findGroundRight = Physics.Raycast(rayRight, out _, rayLength);
         bool findGroundLeft = Physics.Raycast(rayLeft, out _, rayLength);
-        Vector3 movePosition = new Vector3((direction.x > 0 && findGroundRight == false) || (direction.x < 0 && findGroundLeft == false) ? 0 : direction.x, direction.y, (direction.z > 0 && findGroundForward == false) || (direction.z < 0 && findGroundBack == false) ? 0 : direction.z);
-
+        
+        // プレイヤーカメラ起点の入力方向
+        Vector3 direction = cameraForward * moveZ + playerCamera.transform.right * moveX;
+        
         // 入力方向を向く処理
-        if (direction.magnitude > 0.01f)
+        if(direction != Vector3.zero)
         {
-            transform.localRotation = Quaternion.LookRotation(direction);
+            Quaternion rot = Quaternion.LookRotation(direction, Vector3.up);
+            rot = Quaternion.Slerp(transform.rotation, rot, 10.0f * Time.deltaTime);
+            transform.rotation = rot;
         }
 
         // 斜め移動時の移動速度の補正
-        var r = Mathf.Sqrt(movePosition.x * movePosition.x + movePosition.z * movePosition.z);
-        var radian = Vector3.Angle(Vector3.right, movePosition) * Mathf.Deg2Rad;
-        r *= (0.7071f + 0.2928f * Mathf.Abs(Mathf.Cos(2 * radian)));
+        Vector2 moveDirection = new Vector2(moveX * Mathf.Sqrt(1 - (moveZ * moveZ) / 2.0f), moveZ * Mathf.Sqrt(1 - (moveX * moveX) / 2.0f));
+        float moveVec = Mathf.Sqrt((moveDirection.x * moveDirection.x) + (moveDirection.y * moveDirection.y));
 
         // 移動処理
-        if (moveZ >= 0)
-        {
-            transform.position += new Vector3(r * Mathf.Cos(radian), 0, r * Mathf.Sin(radian)) * playerSpeed * Time.deltaTime;
-        }
-        else
-        {
-            transform.position += new Vector3(r * Mathf.Cos(radian), 0, -r * Mathf.Sin(radian)) * playerSpeed * Time.deltaTime;
-        }
+        playerVec = moveVec * transform.forward;
+        playerVec = new Vector3((moveX > 0 && findGroundRight == false) || (moveX < 0 && findGroundLeft == false) ? 0 : playerVec.x, 0, (moveZ > 0 && findGroundForward == false) || (moveZ < 0 && findGroundBack == false) ? 0 : playerVec.z);
     }
 
     /// <summary>
@@ -68,6 +77,22 @@ public class PlayerController : MonoBehaviour
     {
         if (rb.useGravity) { return; }
         rb.AddForce(localGravity, ForceMode.Acceleration);
+    }
+
+    /// <summary>
+    /// プレイヤーの移動処理
+    /// </summary>
+    private void PlayerMove()
+    {
+        if (inputX)
+        {
+            rb.position += new Vector3(playerVec.x, 0, 0) * playerSpeed * Time.fixedDeltaTime;
+        }
+
+        if (inputZ)
+        {
+            rb.position += new Vector3(0, 0, playerVec.z) * playerSpeed * Time.fixedDeltaTime;
+        }
     }
     
     // Start is called before the first frame update
@@ -79,7 +104,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        PlayerMove();
+        PlayerMoveControl();
     }
 
     /// <summary>
@@ -88,6 +113,7 @@ public class PlayerController : MonoBehaviour
     private void FixedUpdate()
     {
         SetLocalGravity();
+        PlayerMove();
     }
 
     /// <summary>
@@ -98,5 +124,7 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
         rb.useGravity = false;
+        rb.interpolation = RigidbodyInterpolation.Interpolate;
+        rb.collisionDetectionMode = CollisionDetectionMode.ContinuousDynamic;
     }
 }
