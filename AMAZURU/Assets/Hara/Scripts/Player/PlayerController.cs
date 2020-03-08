3c31b99bc3b678af8a368bdf125840bb9f13e9de
Kazunori Hara <rize.game.0816@gmail.com>
2020-03-09 08:03:42 +0900

    [Update]プレイヤーの移動処理を修正

--- AMAZURU/Assets/Hara/Scripts/Player/PlayerController.cs
+++ AMAZURU/Assets/Hara/Scripts/Player/PlayerController.cs
@@ -6,29 +6,19 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField, Tooltip("PlayerのRigidbody")] private Rigidbody rb = null;
-    [SerializeField, Header("移動速度"), Range(1, 10)] private float playerSpeed = 1.0f;
+    [SerializeField, Header("プレイヤーの移動速度"), Range(0, 5)] private float playerSpeed = 0;
     [SerializeField, Header("移動時の起点カメラ")] private Camera playerCamera = null;
+    [SerializeField, Header("RayのLayerMask")] private LayerMask layerMask;
+    [SerializeField, Header("Rayの長さ"), Range(0, 3)] private float rayLength = 0.5f;
+    [SerializeField, Header("足の位置"), Range(0, 1)] private float footHeight = 0;
     public Camera PlayerCamera { set { playerCamera = value; } }
 
-    [SerializeField] private Vector3 playerVec = Vector3.zero;
+    // コントローラーの入力値
     private float inputX = 0;
     private float inputZ = 0;
 
-    // 自分の四方に床が存在するか
-    int layerMask = 0;
-    private bool forwardGround = false;
-    private bool backGround = false;
-    private bool rightGround = false;
-    private bool leftGround = false;
-
-    [SerializeField, Tooltip("透明な壁オブジェクト")] private GameObject hideWallObject = null;
-    private BoxCollider wallForward = null;
-    private BoxCollider wallBack = null;
-    private BoxCollider wallRight = null;
-    private BoxCollider wallLeft = null;
-
-    // 目の前に壁が存在するか
-    private bool findWallForward = false;
+    // Y軸方向
+    private float Yangle = 90;
 
     /// <summary>
     /// 初期化
@@ -36,18 +26,12 @@ public class PlayerController : MonoBehaviour
     private void PlayerInit()
     {
         if(playerCamera == null) { playerCamera = Camera.main; }
-        layerMask = ~(1 << LayerMask.NameToLayer("Mirror"));
-
-        wallForward = Instantiate(hideWallObject, transform.position, Quaternion.identity).GetComponent<BoxCollider>();
-        wallBack = Instantiate(hideWallObject, transform.position, Quaternion.identity).GetComponent<BoxCollider>();
-        wallRight = Instantiate(hideWallObject, transform.position, Quaternion.identity).GetComponent<BoxCollider>();
-        wallLeft = Instantiate(hideWallObject, transform.position, Quaternion.identity).GetComponent<BoxCollider>();
     }
 
     /// <summary>
-    /// 移動を管理する
+    /// コントローラ入力を取得
     /// </summary>
-    private void PlayerMoveControl()
+    private void GetInputController()
     {
         // 入力値を取得
         inputX = Input.GetAxis("Horizontal");
@@ -57,132 +41,106 @@ public class PlayerController : MonoBehaviour
     /// <summary>
     /// プレイヤーの移動処理
     /// </summary>
-    private void PlayerMove()
+    private void PlayerMove(bool fixedUpdate)
     {
-        // カメラの向いている方向を取得
-        Vector3 cameraForward = Vector3.Scale(playerCamera.transform.forward, new Vector3(1, 0, 1)).normalized;
+        float delta = fixedUpdate ? Time.fixedDeltaTime : Time.deltaTime;
 
-        // プレイヤーカメラ起点の入力方向
-        Vector3 direction = cameraForward * inputZ + playerCamera.transform.right * inputX;
+        // 入力の最低許容値
+        float inputMin = 0.1f;
 
-        // 入力方向を向く処理
-        if (direction != Vector3.zero)
+        // プレイヤーカメラの向いているY軸方向
+        float playerCameraAngle = -playerCamera.transform.eulerAngles.y;
+
+        // 入力を検知したら実行
+        if(Mathf.Abs(inputX) > inputMin || Mathf.Abs(inputZ) > inputMin)
         {
+            // 向きをプレイヤーカメラから見た入力方向へ修正
+            float refAngle = 0;
+            Yangle = Mathf.SmoothDampAngle(Yangle, Mathf.Atan2(inputZ, inputX) * Mathf.Rad2Deg + playerCameraAngle, ref refAngle, 0.05f);
+
+            // カメラの向いている方向を取得
+            Vector3 cameraForward = Vector3.Scale(playerCamera.transform.forward, new Vector3(1, 0, 1)).normalized;
+
+            // プレイヤーカメラ起点の入力方向
+            Vector3 direction = cameraForward * inputZ + playerCamera.transform.right * inputX;
+
+            // 入力方向を向く処理
             Quaternion rot = Quaternion.LookRotation(direction, Vector3.up);
-            rot = Quaternion.Slerp(transform.rotation, rot, 10.0f * Time.fixedDeltaTime);
+            rot = Quaternion.Slerp(transform.rotation, rot, 15 * delta);
             transform.rotation = rot;
-        }
 
-        // 床が存在するかをRayでチェック
-        RaycastHit hit;
-        if (Physics.SphereCast(new Ray(transform.position, -transform.up), 0.1f, out hit))
-        {
-            float radius = 0.1f;
-            forwardGround = false;
-            backGround = false;
-            rightGround = false;
-            leftGround = false;
-
-            //forwardGround = Physics.SphereCast(new Ray(transform.position + Vector3.forward + transform.up * hit.distance, -transform.up), radius, hit.distance * 2.5f, layerMask);
-            //backGround = Physics.SphereCast(new Ray(transform.position + Vector3.back + transform.up * hit.distance, -transform.up), radius, hit.distance * 2.5f, layerMask);
-            //rightGround = Physics.SphereCast(new Ray(transform.position + Vector3.right + transform.up * hit.distance, -transform.up), radius, hit.distance * 2.5f, layerMask);
-            //leftGround = Physics.SphereCast(new Ray(transform.position + Vector3.left + transform.up * hit.distance, -transform.up), radius, hit.distance * 2.5f, layerMask);
-
-            foreach(RaycastHit raycast in Physics.SphereCastAll(new Ray(transform.position + Vector3.forward + transform.up * hit.distance, -transform.up), radius, hit.distance * 3.5f, layerMask))
+            // Rayを飛ばして進めるかをチェック
+            float angleLate = 1;
+            float forwardAngle = Yangle;
+            Ray playerAround = new Ray(transform.position + new Vector3(Mathf.Cos(forwardAngle * Mathf.Deg2Rad), 0, Mathf.Sin(forwardAngle * Mathf.Deg2Rad)) * playerSpeed * delta, Vector3.down);
+            if (Physics.Raycast(playerAround, rayLength, layerMask) == false)
             {
-                if(raycast.transform.gameObject != wallForward.gameObject)
+                angleLate = 0;
+                for (float f = 0; f < 90; f += 10)
                 {
-                    forwardGround = true;
-                    break;
+                    bool flag = false;
+                    playerAround = new Ray(transform.position + new Vector3(Mathf.Cos((f + Yangle) * Mathf.Deg2Rad), 0, Mathf.Sin((f + Yangle) * Mathf.Deg2Rad)) * playerSpeed * delta, Vector3.down);
+                    if (Physics.Raycast(playerAround, rayLength, layerMask))
+                    {
+                        forwardAngle += f;
+                        flag = true;
+                    }
+                    playerAround = new Ray(transform.position + new Vector3(Mathf.Cos((Yangle - f) * Mathf.Deg2Rad), 0, Mathf.Sin((Yangle - f) * Mathf.Deg2Rad)) * playerSpeed * delta, Vector3.down);
+                    if (Physics.Raycast(playerAround, rayLength, layerMask))
+                    {
+                        forwardAngle -= f;
+                        flag = true;
+                    }
+                    if (flag)
+                    {
+                        angleLate = Mathf.Cos(f * Mathf.Deg2Rad);
+                        break;
+                    }
                 }
             }
 
-            foreach (RaycastHit raycast in Physics.SphereCastAll(new Ray(transform.position + Vector3.back + transform.up * hit.distance, -transform.up), radius, hit.distance * 3.5f, layerMask))
+            // 坂道にRayを飛ばして進めるかチェック
+            float Xangle = 0;
+            Ray playerForward = new Ray(transform.position + Vector3.down * footHeight, new Vector3(Mathf.Cos(forwardAngle * Mathf.Deg2Rad), 0, Mathf.Sin(forwardAngle * Mathf.Deg2Rad)));
+            if (Physics.Raycast(playerForward, angleLate * playerSpeed * delta, layerMask))
             {
-                if (raycast.transform.gameObject != wallBack.gameObject)
+                for (float f = 0; f < 90; f += 5)
                 {
-                    backGround = true;
-                    break;
+                    playerForward = new Ray(transform.position + Vector3.down * footHeight, new Vector3(Mathf.Cos(forwardAngle * Mathf.Deg2Rad) * Mathf.Cos(f * Mathf.Deg2Rad), Mathf.Sin(f * Mathf.Deg2Rad), Mathf.Sin(forwardAngle * Mathf.Deg2Rad) * Mathf.Cos(f * Mathf.Deg2Rad)));
+                    if (Physics.Raycast(playerForward, angleLate * playerSpeed * delta, layerMask) == false)
+                    {
+                        Xangle = f;
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                for (float f = 0; f > -90; f -= 5)
+                {
+                    playerForward = new Ray(transform.position + Vector3.down * footHeight, new Vector3(Mathf.Cos(forwardAngle * Mathf.Deg2Rad) * Mathf.Cos(f * Mathf.Deg2Rad), Mathf.Sin(f * Mathf.Deg2Rad), Mathf.Sin(forwardAngle * Mathf.Deg2Rad) * Mathf.Cos(f * Mathf.Deg2Rad)));
+                    if (Physics.Raycast(playerForward, angleLate * playerSpeed * delta, layerMask))
+                    {
+                        Xangle = f + 5;
+                        break;
+                    }
                 }
             }
 
-            foreach (RaycastHit raycast in Physics.SphereCastAll(new Ray(transform.position + Vector3.right + transform.up * hit.distance, -transform.up), radius, hit.distance * 3.5f, layerMask))
+            // Rayを飛ばしてプレイヤーの移動先座標を決定する
+            Vector3 movePosition;
+            Ray playerFoot = new Ray(transform.position + new Vector3(Mathf.Cos(forwardAngle * Mathf.Deg2Rad) * Mathf.Cos(Xangle * Mathf.Deg2Rad), Mathf.Sin(Xangle * Mathf.Deg2Rad), Mathf.Sin(forwardAngle * Mathf.Deg2Rad) * Mathf.Cos(Xangle * Mathf.Deg2Rad)) * playerSpeed * delta * angleLate, Vector3.down);
+            if (Physics.Raycast(playerFoot, Mathf.Sin(Xangle * Mathf.Deg2Rad) * playerSpeed * delta * angleLate, layerMask))
             {
-                if (raycast.transform.gameObject != wallRight.gameObject)
-                {
-                    rightGround = true;
-                    break;
-                }
-            }
-
-            foreach (RaycastHit raycast in Physics.SphereCastAll(new Ray(transform.position + Vector3.left + transform.up * hit.distance, -transform.up), radius, hit.distance * 3.5f, layerMask))
-            {
-                if (raycast.transform.gameObject != wallLeft.gameObject)
-                {
-                    leftGround = true;
-                    break;
-                }
-            }
-
-            if (forwardGround == false)
-            {
-                wallForward.transform.position = new Vector3(transform.position.x, transform.position.y, wallForward.transform.position.z);
-                wallForward.size = transform.localScale;
-                wallForward.gameObject.SetActive(true);
+                movePosition = new Vector3(Mathf.Cos(forwardAngle * Mathf.Deg2Rad) * Mathf.Cos(Xangle * Mathf.Deg2Rad), 0, Mathf.Sin(forwardAngle * Mathf.Deg2Rad) * Mathf.Cos(Xangle * Mathf.Deg2Rad));
             }
             else
             {
-                wallForward.transform.position = transform.position + Vector3.forward * 1.2f;
-                wallForward.gameObject.SetActive(false);
+                movePosition = new Vector3(Mathf.Cos(forwardAngle * Mathf.Deg2Rad) * Mathf.Cos(Xangle * Mathf.Deg2Rad), Mathf.Sin(Xangle * Mathf.Deg2Rad), Mathf.Sin(forwardAngle * Mathf.Deg2Rad) * Mathf.Cos(Xangle * Mathf.Deg2Rad));
             }
 
-            if (backGround == false)
-            {
-                wallBack.transform.position = new Vector3(transform.position.x, transform.position.y, wallBack.transform.position.z);
-                wallBack.size = transform.localScale;
-                wallBack.gameObject.SetActive(true);
-            }
-            else
-            {
-                wallBack.transform.position = transform.position + Vector3.back * 1.2f;
-                wallBack.gameObject.SetActive(false);
-            }
-
-            if (rightGround == false)
-            {
-                wallRight.transform.position = new Vector3(wallRight.transform.position.x, transform.position.y, transform.position.z);
-                wallRight.size = transform.localScale;
-                wallRight.gameObject.SetActive(true);
-            }
-            else
-            {
-                wallRight.transform.position = transform.position + Vector3.right * 1.2f;
-                wallRight.gameObject.SetActive(false);
-            }
-
-            if (leftGround == false)
-            {
-                wallLeft.transform.position = new Vector3(wallLeft.transform.position.x, transform.position.y, transform.position.z);
-                wallLeft.size = transform.localScale;
-                wallLeft.gameObject.SetActive(true);
-            }
-            else
-            {
-                wallLeft.transform.position = transform.position + Vector3.left * 1.2f;
-                wallLeft.gameObject.SetActive(false);
-            }
-
-            // 壁が存在するかRayでチェック
-            findWallForward = Physics.BoxCast(transform.position, new Vector3(0.5f * transform.localScale.x, 0.5f * transform.localScale.y, 0.5f * transform.localScale.z) * 0.5f, transform.forward, transform.rotation, hit.distance * 0.5f);
-        }
-
-        // 移動処理
-        if ((Mathf.Abs(inputX) > 0.1f || Mathf.Abs(inputZ) > 0.1f) && findWallForward == false)
-        {
-            float moveVec = Mathf.Abs(inputX) >= Mathf.Abs(inputZ) ? inputZ / inputX : inputX / inputZ;
-            moveVec = 1.0f / Mathf.Sqrt(1.0f + moveVec * moveVec);
-            playerVec = direction * moveVec;
-            rb.position += playerVec * playerSpeed * Time.fixedDeltaTime;
+            // プレイヤーを移動させる
+            rb.position += movePosition * playerSpeed * delta * angleLate;
         }
     }
     
@@ -195,7 +153,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        PlayerMoveControl();
+        GetInputController();
     }
 
     /// <summary>
@@ -203,7 +161,7 @@ public class PlayerController : MonoBehaviour
     /// </summary>
     private void FixedUpdate()
     {
-        PlayerMove();
+        PlayerMove(true);
     }
 
     /// <summary>
