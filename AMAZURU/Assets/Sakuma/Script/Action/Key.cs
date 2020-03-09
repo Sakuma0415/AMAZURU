7db2fe47a21cfb46efdb4f15bc13950102dea942
sakuma <syoma.0415@gmail.com>
2020-03-09 15:42:46 +0900

    げ

---
+++ AMAZURU/Assets/Sakuma/Script/Action/Key.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour
+{
+    bool anime = false;
+    float animeTime = 0;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (LayerMask.LayerToName(other.gameObject.layer) == "Player")
+        {
+            anime = true;
+            animeTime = 1;
+            Progress.progress.key = true;
+            GetComponent<Animator>().SetBool("set", true);
+        }
+    }
+    private void Update()
+    {
+        if (anime)
+        {
+            animeTime -= Time.deltaTime;
+            if (animeTime<0)
+            {
+                gameObject.SetActive(false);
+            }
+        }
+    }
+}
