6b320c717d943bfd6719a7b09b6b75796875836e
sigkalra <sigkalra8888@gmail.com>
2020-03-17 13:28:30 +0900

    [Update]Scenemanagerにpublic関数追加[ LoadScene(SceneName name) ]

---
+++ AMAZURU/Assets/Shimojima/Scripts/BB.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BB : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Scenemanager.Instance.LoadScene(Scenemanager.SceneName.StageEdit);
+        }
+
+    }
+}
