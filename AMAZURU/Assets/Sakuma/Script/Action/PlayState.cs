47ed80582c8b755e0035fa554a985ebf65c76a80
sakuma <syoma.0415@gmail.com>
2020-03-20 23:04:26 +0900

    [New] 雨降らすオブジェ作成

--- AMAZURU/Assets/Sakuma/Script/Action/PlayState.cs
+++ AMAZURU/Assets/Sakuma/Script/Action/PlayState.cs
@@ -5,20 +5,88 @@ using UnityEngine;
 public class PlayState : MonoBehaviour
 {
     static public PlayState playState;
+    public bool copyFlg = false; 
     public enum GameMode
     {
         Play,
         Menu,
         Anime,
-        Stop
+        Stop,
+        Rain,
+        RainSelect
     }
+    GameMode backGameMode;
     public GameMode gameMode;
+
+    float rainTime = 0;
+
+
+
+
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
+
         playState = new PlayState();
         playState.gameMode = GameMode.Play;
+
+        if (!copyFlg)
+        {
+            DontDestroyOnLoad(gameObject);
+            playState.copyFlg = true;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
     }
 
+
+
+
+    private void Update()
+    {
+        Debug.Log(playState.gameMode);
+        if(playState.backGameMode != playState.gameMode)
+        {
+            switch (playState.gameMode)
+            {
+                case GameMode.Rain:
+                    Debug.Log("s");
+                    playState.rainTime = 2;
+                    break;
+            }
+
+        }
+        playState.backGameMode = playState.gameMode;
+
+        switch (playState.gameMode)
+        {
+            case GameMode.Rain:
+                playState.RainUpDate();
+                break;
+        }
+
+
+
+
+
+    }
+
+
+    void RainUpDate()
+    {
+        //Debug.Log(playState.rainTime);
+        playState.rainTime -= Time.deltaTime;
+        if (playState.rainTime <= 0)
+        {
+            playState.gameMode = GameMode.Play;
+        }
+    }
 
 }
--- AMAZURU/Assets/Sakuma/Script/Test/WaterHi.cs
+++ AMAZURU/Assets/Sakuma/Script/Test/WaterHi.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class WaterHi : MonoBehaviour
 {
     public float max=0.25f;
+    float back;
+    float next;
+    bool rainAnime = false;
+    float animeTime = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,16 +17,65 @@ public class WaterHi : MonoBehaviour
 
     private void FixedUpdate()
     {
+        if (rainAnime)
+        {
+            animeTime -= Time.fixedDeltaTime;
+            if (animeTime < 1.5f && animeTime > 0.5f)
+            {
+                //transform.localScale = new Vector3(transform.localScale.x, Mathf.Lerp(back, next,1-(animeTime -0.5f)), transform.localScale.z);
+                max = Mathf.Lerp(back, next, 1 - (animeTime - 0.5f));
+            }
+            if (animeTime < 0.5f)
+            {
+                //transform.localScale = new Vector3(transform.localScale.x, next, transform.localScale.z);
+                max = next;
+            }
+            if (animeTime <= 0)
+            {
+                rainAnime = false;
+            }
+        }
+
+
+
+
+
+
+
+
+
         transform.localScale = new Vector3(transform.localScale.x, max, transform.localScale.z);
     }
     // Update is called once per frame
     void Update()
     {
 
-        if (Input.GetKey(KeyCode.U)) { max += Time.deltaTime; }
-        if (Input.GetKey(KeyCode.I)) { max -= Time.deltaTime; }
+        if (Input.GetKeyDown(KeyCode.U)) { HiChange(4); }
+        //if (Input.GetKey(KeyCode.I)) { max -= Time.deltaTime; }
 
-            
+
+
+
+
         
     }
+
+    public void HiChange(float nextHi)
+    {
+        if (!rainAnime)
+        {
+            PlayState.playState.gameMode = PlayState.GameMode.Rain;
+            back = transform.localScale.y;
+            next = nextHi-0.1f;
+            rainAnime = true;
+            animeTime = 2;
+        }
+
+
+    }
+
+
+
+
+
 }
