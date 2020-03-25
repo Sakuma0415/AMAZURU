bd4328f344b184ab20fee876f4bdd1c49a97665e
sakuma <syoma.0415@gmail.com>
2020-03-25 20:52:02 +0900

    [Update] いろいろ更新

--- AMAZURU/Assets/Sakuma/Script/Action/InCamera.cs
+++ AMAZURU/Assets/Sakuma/Script/Action/InCamera.cs
@@ -10,19 +10,25 @@ public class InCamera : MonoBehaviour
     [SerializeField]
     Image image;
     float time = 0;
+    [SerializeField]
+    LayerMask inMask;
+    [SerializeField]
+    LayerMask outMask;
+    [SerializeField]
+    Camera camera;
     // Start is called before the first frame update
     void Start()
     {
-        
+        image.color = new Color(image.color.r, image.color.g, image.color.b, 0);
     }
 
     // Update is called once per frame
     private void FixedUpdate()
     {
-
-        maskObj.SetActive(set);
-        time = (set ? (time + Time.fixedDeltaTime > 0.1f ? 0.1f : time + Time.fixedDeltaTime) : (time - Time.fixedDeltaTime < 0f ? 0 : time - Time.fixedDeltaTime));
-        image.color = new Color(image.color.r, image.color.g, image.color.b, time * 2f+0.1f);
+        camera.cullingMask = !set ? outMask : inMask;
+        //maskObj.SetActive(set);
+        time = (set ? 0.1f : (time - Time.fixedDeltaTime < 0f ? 0 : time - Time.fixedDeltaTime));
+        image.color = new Color(image.color.r, image.color.g, image.color.b, (time==0)?0:time *2+0.3f);
     }
     private void OnTriggerEnter(Collider other)
     {
