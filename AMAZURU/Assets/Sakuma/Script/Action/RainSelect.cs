31172a37ccdb8fb13ecd012004bdafc227804593
sakuma <syoma.0415@gmail.com>
2020-04-03 15:03:07 +0900

    [Update] アメフラシの仕様変更

---
+++ AMAZURU/Assets/Sakuma/Script/Action/RainSelect.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RainSelect : MonoBehaviour
+{
+    
+    void Update()
+    {
+        if (PlayState.playState.gameMode == PlayState.GameMode.RainSelect)
+        {
+
+        }
+    }
+}
