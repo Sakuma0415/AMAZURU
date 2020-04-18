47b1fd87db6d284e09fc7baf25ca5baa2a51fd4a
sigkalra <sigkalra8888@gmail.com>
2020-04-18 19:51:55 +0900

    [Update]まだ逆回転対応中

--- AMAZURU/Assets/Shimojima/Scripts/StageSelect/Select.cs
+++ AMAZURU/Assets/Shimojima/Scripts/StageSelect/Select.cs
@@ -19,6 +19,7 @@ public class Select : MonoBehaviour
     private float speed = 1;
     [SerializeField]
     private int rotateAngle;
+    private float sumAngle;
     private bool isRotation = false;
     
     private enum Selection
@@ -41,9 +42,37 @@ public class Select : MonoBehaviour
         public Vector3 reSizeSpeed;
         public Vector3 zeroScalingSpeed;
         public int index;
+        public int psdIndex;
+        public void Init()
+        {
+            name = "";
+            stage = null;
+            defScale = Vector3.zero;
+            reSizeSpeed = Vector3.zero;
+            zeroScalingSpeed = Vector3.zero;
+            index = 0;
+            psdIndex = 0;
+        }
+
+        public void TookOver(ViewStage v)
+        {
+            name = v.name;
+            stage = v.stage;
+            defScale = v.defScale;
+            reSizeSpeed = v.reSizeSpeed;
+            zeroScalingSpeed = v.zeroScalingSpeed;
+            index = v.index;
+            psdIndex = v.psdIndex;
+        }
+
+        public void CountUp()
+        {
+            index++;
+            if(index == 6) { index = 0; }
+        }
     }
 
-    public ViewStage[] viewStage = new ViewStage[5];
+    public ViewStage[] viewStage = new ViewStage[6];
 
     
     void Start()
@@ -53,34 +82,67 @@ public class Select : MonoBehaviour
 
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            if (isRotation) { return; }
+            selection = Selection.Forwerd;
+            isRotation = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            if (isRotation) { return; }
+            selection = Selection.FallBack;
+            isRotation = true;
+        }
     }
 
+    private void FixedUpdate()
+    {
+        if (!isRotation) { return; }
+        StageRotate(selection);
+    }
+
+    /// <summary>
+    /// 初期化
+    /// </summary>
     private void Init()
     {
         psd = Resources.LoadAll<PrefabStageData>("EditData/");
         int overCount = 1;
-        for (int i = 0; i < 4; i++)
+        for (int i = 0; i < 5; i++)
         {
             if(i > (psd.Length - 1) * overCount)
             {
                 int _i = i - psd.Length;
-                stages.Add(psd[_i].stage);
+                viewStage[i].stage = Instantiate(psd[_i].stage);
                 StageReSize(psd[_i], i);
+                viewStage[i].psdIndex = _i;
             }
             else
             {
-                stages.Add(psd[i].stage);
+                viewStage[i].stage = Instantiate(psd[i].stage);
                 StageReSize(psd[i], i);
+                viewStage[i].psdIndex = i;
             }
 
-            viewStage[i].stage = Instantiate(stages[i]);
+            
             viewStage[i].name = "shimojima1";
             viewStage[i].stage.transform.localScale = viewStage[i].defScale;
             SetScaleChangeSpeed(i);
             viewStage[i].stage.transform.position = defPos;
-            viewStage[i].stage.transform.RotateAround(senterPivot.transform.position, Vector3.up, rotateAngle * (i + 1));
+            viewStage[i].stage.transform.RotateAround(senterPivot.transform.position, Vector3.up, rotateAngle * (i + 8));
             viewStage[i].index = i;
+            if(i == 2) 
+            {
+                if (viewStage[i].defScale != Vector3.one)
+                {
+                    viewStage[i].stage.transform.localScale = new Vector3(0.8f, 1, 0.8f);
+                }
+                else
+                {
+                    viewStage[i].stage.transform.localScale = new Vector3(1.5f, 1, 1.5f);
+                }
+            }
         }
     }
 
@@ -125,6 +187,10 @@ public class Select : MonoBehaviour
             Vector2 reSize = new Vector2((x * magni) / scale.x, (z * magni) / scale.y);
             viewStage[i].defScale = new Vector3(reSize.x, 1, reSize.y);
         }
+        else
+        {
+            viewStage[i].defScale = Vector3.one;
+        }
     }
 
     /// <summary>
@@ -148,5 +214,153 @@ public class Select : MonoBehaviour
 
         Vector3 _scale = viewStage[i].stage.transform.localScale;
         viewStage[i].zeroScalingSpeed = new Vector3(_scale.x / rotateAngle, _scale.y / rotateAngle, _scale.z / rotateAngle);
+    }
+
+    /// <summary>
+    /// ステージの回転
+    /// </summary>
+    private void StageRotate(Selection select)
+    {
+        StageInstantiate(select);
+
+        for (int i = 0; i < viewStage.Length; i++)
+        {
+            float s = speed;
+            if(select == Selection.FallBack) { s *= -1; }
+
+            if(select == Selection.Forwerd)
+            {
+                switch (viewStage[i].index) 
+                {
+                    case 1:
+                        viewStage[i].stage.transform.localScale += viewStage[i].reSizeSpeed;
+                        break;
+                    case 2:
+                        viewStage[i].stage.transform.localScale -= viewStage[i].reSizeSpeed;
+                        break;
+                    case 4:
+                        viewStage[i].stage.transform.localScale -= viewStage[i].zeroScalingSpeed;
+                        break;
+                    case 5:
+                        viewStage[i].stage.transform.localScale += viewStage[i].zeroScalingSpeed;
+                        break;
+                }
+            }
+            if(viewStage[i].stage == null) { continue; }
+            viewStage[i].stage.transform.RotateAround(senterPivot.transform.position, Vector3.up, s) ;
+        }
+
+        sumAngle += speed;
+
+        if (sumAngle >= rotateAngle)
+        {
+            sumAngle = 0;
+            isRotation = false;
+            StageDataChange(select);
+        }
+    }
+
+    /// <summary>
+    /// データ変更
+    /// </summary>
+    /// <param name="select"></param>
+    private void StageDataChange(Selection select)
+    {
+        if (select == Selection.Forwerd)
+        {
+            for (int i = 0; i < viewStage.Length; i++)
+            {
+                if(viewStage[i].index == 4)
+                {
+                    Destroy(viewStage[i].stage);
+                    viewStage[i].Init();
+                }
+                
+                if(viewStage[i].stage == null) { continue; }
+                viewStage[i].CountUp();
+            }
+        }
+    }
+
+    /// <summary>
+    /// ステージのインスタンス作成
+    /// </summary>
+    private void StageInstantiate(Selection select)
+    {
+        if(select == Selection.Forwerd && sumAngle == 0)
+        {
+            if (viewStage[5].stage != null)
+            {
+                int x = 4;
+
+                while (true)
+                {
+                    viewStage[x].TookOver(viewStage[x - 1]);
+                    x--;
+                    if (x == 0)
+                    {
+                        viewStage[x].TookOver(viewStage[viewStage.Length - 1]);
+                        viewStage[viewStage.Length - 1].Init();
+                        break;
+                    }
+                }
+
+                NextStageCreate(select);
+            }
+            else
+            {
+                NextStageCreate(select);
+            }
+        }
+        else if(select == Selection.FallBack && sumAngle == 0)
+        {
+            if(viewStage[4].stage != null)
+            {
+                int x = 0;
+
+                while (true)
+                {
+                    if (x == 0)
+                    {
+                        viewStage[viewStage.Length - 1].TookOver(viewStage[x]);
+                    }
+                    x++;
+                    viewStage[x - 1].TookOver(viewStage[x]);
+                    if (x == 4) { viewStage[x].Init(); break; }
+                }
+
+                NextStageCreate(select);
+            }
+        }
+    }
+
+    /// <summary>
+    /// 次のステージインスタンスを作成
+    /// </summary>
+    /// <param name="select"></param>
+    private void NextStageCreate(Selection select)
+    {
+        if (select == Selection.Forwerd)
+        {
+            int index = viewStage[0].psdIndex + 1;
+            if (index > psd.Length - 1) { index = 0; }
+
+            viewStage[5].stage = Instantiate(psd[index].stage);
+            viewStage[5].name = "shiomojima1";
+            viewStage[5].index = 5;
+            viewStage[5].psdIndex = index;
+            StageReSize(psd[index], 5);
+            viewStage[5].stage.transform.localScale = viewStage[5].defScale;
+            viewStage[5].stage.transform.position = defPos;
+            SetScaleChangeSpeed(5);
+            viewStage[5].stage.transform.localScale = Vector3.zero;
+            viewStage[5].stage.transform.RotateAround(senterPivot.transform.position, Vector3.up, rotateAngle * 7);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawCube(senterPivot.transform.position, pivotCubeSize);
     }
 }
