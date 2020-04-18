f0639822e28bfd5aaf1e1bdf5c0e6c1d9bc0d7de
sakuma <syoma.0415@gmail.com>
2020-04-18 17:19:49 +0900

    [Update] いろいろ

---
+++ AMAZURU/Assets/Sakuma/Script/AmeFurashiAmount.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class AmeFurashiAmount : MonoBehaviour
+{
+    [SerializeField]
+    Text text1, text2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text1.text = AmehurashiManager.amehurashi.amehurashiTrueCont.ToString ();
+        text2.text = AmehurashiManager.amehurashi.AmehurashiQuantity.ToString();
+    }
+}
