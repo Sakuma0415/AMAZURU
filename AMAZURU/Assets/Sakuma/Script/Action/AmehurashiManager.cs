d78d113e12393133f2750e412a3a677e1d2b6350
sakuma <syoma.0415@gmail.com>
2020-04-25 01:59:57 +0900

    [毎日Scriptを1つコメントを書こうのコーナー1日目] カメラ

--- AMAZURU/Assets/Sakuma/Script/Action/AmehurashiManager.cs
+++ AMAZURU/Assets/Sakuma/Script/Action/AmehurashiManager.cs
@@ -2,25 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+
+/// <summary>
+/// アメフラシ全体で共有する情報のクラス
+/// </summary>
 public class AmehurashiManager : MonoBehaviour
 {
+    //アクセス先の静的オブジェ
     static public AmehurashiManager amehurashi;
-    public WaterHi waterHi;
 
+    [Header("StageDataからの読み込みのため以下変更不可")]
+    //水位の高さ
+    public WaterHi waterHi;
+    //アメフラシ一つで上昇する水位の高さ
+    public float waterStep = 1;
+    //ステージ上に存在するアメフラシの数
+    public int AmehurashiQuantity = 0;
+    //アメフラシを起動している数
+    public int amehurashiTrueCont = 0;
+    
+
+    //+　初期化
     public void ManagerSet()
     {
         amehurashi = this;
     }
-    public float waterStep = 1;
+
+    //凍結中
+    /*
+        以下アメフラシ起動時にウィンドウを表示する関数
+        未使用だが参考、再使用の可能性も考慮し保持
+    */
+#if false
     public float hi;
-    public int amehurashiTrueCont = 0;
-    public int AmehurashiQuantity = 0;
     public RainPot rainPot;
     [SerializeField]
     float[] hiList = new float[0];
 
-    //凍結中
-#if false
     public void SwOn()
     {
         if (!rainPot.sw)
@@ -95,8 +113,4 @@ public class AmehurashiManager : MonoBehaviour
         PlayState.playState.gameMode = PlayState.GameMode.Play;
     }
 #endif
-
-
-
-
 }
--- AMAZURU/Assets/Sakuma/Script/Action/CameraPos.cs
+++ AMAZURU/Assets/Sakuma/Script/Action/CameraPos.cs
@@ -1,86 +1,113 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/// <summary>
+/// カメラの管理クラス
+/// </summary>
 public class CameraPos : MonoBehaviour
 {
-    public float CameraDisS;
-    public float CameraDisP;
-
-    [SerializeField]
-    float CameraDis;
-
-    public Vector3 lookPos;
-    public Transform PlayerTransform;
-
-
-    private Vector3 lookObj;
-
-    public float XZangle = 0;
-    [Range(0,90)]
-    [SerializeField]
+    [Header ("設定項目")]
+    //X-Z平面に垂直に交わる平面の注視点から見たカメラの角度
+    [SerializeField ,Range(0, 90)]
     private float Yangle = 0;
-
-    float targetAngle;
-    float angleSpeed;
-    [SerializeField]
-    bool lookMode = false;
-    float lookAnimeTime = 0;
-
-    Vector3 animePos;
-    Vector3 animeRef;
-    [SerializeField]
-    bool MouseHack = true;
-
-
-    [SerializeField]
-    [Range(-10, 10)]
+    //注視点を画面下部にずらす量
+    [SerializeField, Range(-10, 10)]
     float lookHi;
-    float newLookHi;
-
+    //注視点変更に使う時間の長さ
     [SerializeField]
     float changeTime;
-
-    [SerializeField]
-    public float fAngle;
-
-    bool MouseCheck = true;
-    [SerializeField]
-    float endCameraPos;
+    //カメラが衝突を検知するマスク
     [SerializeField]
     LayerMask layerMask;
+    //カメラのコライダー
     [SerializeField]
     SphereCollider sphereCollider;
 
+    [Header("以下変更不可")]
+    //ステージ注視時のカメラ、ステージ間の距離
+    public float CameraDisS;
+    //プレイヤー注視時のカメラ、プレイヤー間の距離
+    public float CameraDisP;
+    //ステージの中心座標
+    public Vector3 lookPos;
+    //プレイヤーのトランスフォーム
+    public Transform PlayerTransform;
+    //ゲーム開始時のカメラの角度
+    public float fAngle;
+    //アメフラシの演出のカメラが移動する時間
+    public float rainPotChangeAnimeTimeSpead;
+
+    //private変数
+    //現在のカメラ、注視点間の距離
+    float CameraDis;
+    //現在の注視点の座標
+    Vector3 lookObj;
+    //X-Z平面上の注視点から見たカメラの角度
+    float XZangle = 0;
+    //注視点　true=プレイヤー：false=ステージ
+    bool lookMode = false;
+    //注視点変更アニメーションの経過時間
+    float lookAnimeTime = 0;
+    //注視点変更アニメーションの開始地点の座標
+    Vector3 animePos;
+    //マウスを固定＆透明化させるかのフラグ
+    bool MouseHack = true;
+    //現在の注視点を画面下部にずらす量
+    float newLookHi;
+    //マウスからの情報を受け取るかどうかのフラグ
+    bool MouseCheck = true;
+    //カメラがステージに埋まった時のカメラ、注視点の距離
+    float endCameraPos;
+    //ゲーム開始時の定点ビューを再生するフラグ
     bool startCameraFlg = true;
+    //開始時の定点ビューから開始時のカメラの角度へリセットさせるフラグ
     bool startCameraAngleResetFlg = true;
+    //開始時の角度リセット時の角度を保存しておくバッファ
     float startCameraAngleResetBf = 0;
-    // Start is called before the first frame update
+    //アメフラシの起動中のフラグ
+    bool rainPotChange = false;
+    //アメフラシの演出終了後にカメラが戻るステータスの格納先
+    float beforeAngleY = 0;
+    float beforeAngleXZ = 0;
+    float beforeDis = 0;
+    float beforeHi = 0;
+    Vector3 beforePos;
+    //アメフラシの演出の経過時間
+    float potAnimeTime;
+    //アメフラシの演出の入りか終わりか
+    bool outflg = false;
+    //アメフラシ起動時に移動する角度
+    float lotAngle = 0;
+
     void Start()
     {
+        //初期化
         startCameraFlg = true;
-        
         XZangle = fAngle;
-        targetAngle = fAngle;
         CameraDis =lookMode ? CameraDisP: CameraDisS;
         MouseCheck = true;
     }
 
     private void LateUpdate()
     {
+
+        //カメラの移動可能なゲームモード＆アメフラシの演出中ではない
         if ((PlayState.playState.gameMode == PlayState.GameMode.Play|| PlayState.playState.gameMode == PlayState.GameMode.StartEf ) || rainPotChange)
         {
+
+            //注視点変更アニメーション中
             if (lookAnimeTime > 0)
             {
-
                 transform.position = Vector3.Lerp(animePos, (new Vector3(Mathf.Cos(XZangle * Mathf.Deg2Rad) * Mathf.Cos(Yangle * Mathf.Deg2Rad), Mathf.Sin(Yangle * Mathf.Deg2Rad) + newLookHi, Mathf.Sin(XZangle * Mathf.Deg2Rad) * Mathf.Cos(Yangle * Mathf.Deg2Rad)) * (!lookMode ? CameraDisS : CameraDisP)) + lookObj, 1 - (lookAnimeTime/ changeTime));
-                
             }
             else
             {
+
+                //注視点がプレイヤー
                 if (lookMode)
                 {
 
+                    //カメラがステージに埋まっているかどうかの処理
                     if (Physics.OverlapSphere((new Vector3(Mathf.Cos(XZangle * Mathf.Deg2Rad) * Mathf.Cos(Yangle * Mathf.Deg2Rad), Mathf.Sin(Yangle * Mathf.Deg2Rad) + newLookHi, Mathf.Sin(XZangle * Mathf.Deg2Rad) * Mathf.Cos(Yangle * Mathf.Deg2Rad)) * CameraDis) + lookObj, sphereCollider.radius, layerMask).Length == 0)
                     {
                         endCameraPos = CameraDis;
@@ -106,19 +133,25 @@ public class CameraPos : MonoBehaviour
                     endCameraPos = CameraDis;
                 }
 
+                //座標移動
                 transform.position = (new Vector3(Mathf.Cos(XZangle * Mathf.Deg2Rad) * Mathf.Cos(Yangle * Mathf.Deg2Rad), Mathf.Sin(Yangle * Mathf.Deg2Rad) + newLookHi, Mathf.Sin(XZangle * Mathf.Deg2Rad) * Mathf.Cos(Yangle * Mathf.Deg2Rad)) * endCameraPos) + lookObj;
             }
 
+            //角度変更
             transform.localEulerAngles = new Vector3(Yangle, -XZangle - 90, 0);
         }
     }
     // Update is called once per frame
     void Update()
     {
+
+        //ゲーム開始時の定点カメラの特殊挙動時のステータス更新
         if (startCameraFlg)
         {
             lookObj = lookMode ? PlayerTransform.position : lookPos;
             XZangle += Time.deltaTime*3;
+
+            //通常のカメラ処理に戻る
             if (Input.GetKeyDown(KeyCode.Space))
             {
                 startCameraAngleResetBf = XZangle;
@@ -130,10 +163,11 @@ public class CameraPos : MonoBehaviour
             }
         }
 
-        //Debug.Log(lookObj); 
-
+        //プレイ中の処理
         if (PlayState.playState.gameMode == PlayState.GameMode.Play)
         {
+
+            //マウスカーソルの設定
             Cursor.visible = !MouseHack;
             if (MouseHack)
             {
@@ -143,54 +177,37 @@ public class CameraPos : MonoBehaviour
             {
                 Cursor.lockState = CursorLockMode.None;
             }
+
+            //マウスカーソルの切り替え
             if (Input.GetKeyDown(KeyCode.Mouse1))
             {
                 MouseHack = !MouseHack;
             }
+
             if (MouseHack)
             {
-
                 if (MouseCheck)
                 {
-                        float mouse_x_delta = Input.GetAxis("Mouse X");
-                        float mouse_y_delta = Input.GetAxis("Mouse Y");
 
-                        XZangle -= mouse_x_delta * (lookMode ? CameraDisS : CameraDisP) / 10;
-                        Yangle -= mouse_y_delta * (lookMode ? CameraDisS : CameraDisP) / 10;
+                    //マウスの移動情報を角度の変更量に変換
+                    float mouse_x_delta = Input.GetAxis("Mouse X");
+                    float mouse_y_delta = Input.GetAxis("Mouse Y");
 
-                        if (Yangle > 90)
-                        {
-                            Yangle = 90;
-                        }
-                        if (Yangle < -90)
-                        {
-                            Yangle = -90;
-                        }
+                    XZangle -= mouse_x_delta * (lookMode ? CameraDisS : CameraDisP) / 10;
+                    Yangle -= mouse_y_delta * (lookMode ? CameraDisS : CameraDisP) / 10;
 
-
+                    if (Yangle > 90)
+                    {
+                        Yangle = 90;
+                    }
+                    if (Yangle < -90)
+                    {
+                        Yangle = -90;
+                    }
                 }
-
-
             }
-            //else
-            //{
-            //    bool right = Input.GetKey(KeyCode.T);
-            //    bool left = Input.GetKey(KeyCode.Y);
-            //    if (right)
-            //    {
-            //        XZangle -= 2;
-            //    }
-            //    if (left)
-            //    {
-            //        XZangle += 2;
-            //    }
-            //}
-
-
-
-
-
-
+            
+            //注視点変更
             if (Input.GetKeyDown(KeyCode.Q) && lookAnimeTime == 0&&!startCameraFlg )
             {
                 lookMode = !lookMode;
@@ -198,6 +215,7 @@ public class CameraPos : MonoBehaviour
                 animePos = transform.position;
             }
 
+            //注視点変更アニメーション中の処理
             if (lookAnimeTime > 0)
             {
                 MouseCheck = false;
@@ -209,6 +227,8 @@ public class CameraPos : MonoBehaviour
                 }
                 CameraDis = Mathf.Lerp(lookMode ? CameraDisS : CameraDisP, lookMode ? CameraDisP : CameraDisS, 1 - (lookAnimeTime/ changeTime));
                 newLookHi = Mathf.Lerp(lookMode ? 0 : lookHi, lookMode ? lookHi  : 0, 1 - (lookAnimeTime / changeTime));
+
+                //ゲーム開始時の定点から通常カメラに戻るときの要素設定
                 if (startCameraAngleResetFlg)
                 {
                     XZangle = Mathf.Lerp(startCameraAngleResetBf  , fAngle , 1 - (lookAnimeTime / changeTime));
@@ -222,23 +242,27 @@ public class CameraPos : MonoBehaviour
                 }
             }
 
+            //注視点の座標を更新
             lookObj = lookMode ? PlayerTransform.position : lookPos;
 
         }
         else
         {
-            Cursor.visible = true ;
 
-                Cursor.lockState = CursorLockMode.None;
-            
+            //プレイ中以外にはマウスの設定を初期化
+            Cursor.visible = true ;
+            Cursor.lockState = CursorLockMode.None;
         }
 
+        //アメフラシの演出中のカメラの処理
         if (rainPotChange)
         {
             potAnimeTime += Time.deltaTime;
 
             if (outflg)
             {
+
+                //カメラの情報を直接上書き
                 if (potAnimeTime < rainPotChangeAnimeTimeSpead)
                 {
                     XZangle = Mathf.LerpAngle(lotAngle, beforeAngleXZ, potAnimeTime/ rainPotChangeAnimeTimeSpead);
@@ -260,6 +284,8 @@ public class CameraPos : MonoBehaviour
             }
             else
             {
+
+                //カメラの情報を直接上書き
                 if (potAnimeTime < rainPotChangeAnimeTimeSpead)
                 {
                     XZangle = Mathf.LerpAngle(beforeAngleXZ, lotAngle, potAnimeTime / rainPotChangeAnimeTimeSpead);
@@ -281,35 +307,21 @@ public class CameraPos : MonoBehaviour
         }
     }
 
-
-    //雨降らし選択時のカメラ遷移用
-    [SerializeField]
-    bool rainPotChange = false;
-    float beforeAngleY = 0;
-    float beforeAngleXZ = 0;
-    float beforeDis = 0;
-    float beforeHi = 0;
-    Vector3 beforePos;
-    float rainPotChangeAnimeTime;
-    public float rainPotChangeAnimeTimeSpead;
-    float potAnimeTime;
-    bool outflg = false;
-    float lotAngle = 0;
-
+    //カメラをアメフラシ起動の状態にする
     public void RainPotChange()
     {
-
+        //  ステータスを初期化
         beforeHi = lookHi;
         rainPotChange = true;
         outflg = false;
-        rainPotChangeAnimeTime = 0;
         beforePos = lookObj;
         beforeAngleXZ = XZangle;
         beforeAngleY = Yangle;
         beforeDis = lookMode ? CameraDisP : CameraDisS;
         potAnimeTime = 0;
-
         float bfAngle = XZangle;
+
+        //角度を0-360に
         while (bfAngle > 360)
         {
             bfAngle -= 360;
@@ -318,6 +330,8 @@ public class CameraPos : MonoBehaviour
         {
             bfAngle += 360;
         }
+
+        //アメフラシ起動中に向かう角度設定
         if(bfAngle < 90)
         {
             lotAngle = 45;
@@ -335,6 +349,8 @@ public class CameraPos : MonoBehaviour
             lotAngle = 315;
         }
     }
+
+    //カメラをアメフラシ終了の状態にする
     public void RainPotChangeOut()
     {
         potAnimeTime = 0;
