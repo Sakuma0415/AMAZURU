442e880c1129ccfc8955a81815385a481d023e47
sakuma <syoma.0415@gmail.com>
2020-05-11 15:53:22 +0900

    カメラ修正

--- AMAZURU/Assets/Sakuma/Script/Action/CameraPos.cs
+++ AMAZURU/Assets/Sakuma/Script/Action/CameraPos.cs
@@ -87,6 +87,8 @@ public class CameraPos : MonoBehaviour
     bool outflg = false;
     //アメフラシ起動時に移動する角度
     float lotAngle = 0;
+    //開始までの時間
+    float startTime = 0;
 
     void Start()
     {
@@ -95,6 +97,7 @@ public class CameraPos : MonoBehaviour
         XZangle = fAngle;
         CameraDis =lookMode ? CameraDisP: CameraDisS;
         MouseCheck = true;
+        startTime = 0;
     }
 
     private void LateUpdate()
@@ -158,11 +161,12 @@ public class CameraPos : MonoBehaviour
         //ゲーム開始時の定点カメラの特殊挙動時のステータス更新
         if (startCameraFlg)
         {
+            startTime += Time.deltaTime;
             lookObj = lookMode ? PlayerTransform.position+new Vector3 (0,LookHiSet,0)  : lookPos;
             XZangle += Time.deltaTime*3;
 
             //通常のカメラ処理に戻る
-            if (Input.GetButtonDown("Circle"))
+            if (Input.GetButtonDown("Circle")&& startTime>1)
             {
                 startCameraAngleResetBf = XZangle;
                 startCameraFlg = false;
@@ -203,8 +207,8 @@ public class CameraPos : MonoBehaviour
                     float mouse_x_delta = Mathf.Abs(Input.GetAxis("Horizontal2"))<0.1f?0: Input.GetAxis("Horizontal2") * stickSpead*Time.deltaTime ;
                     float mouse_y_delta = Mathf.Abs( Input.GetAxis("Vertical2")) < 0.1f ? 0 : Input.GetAxis("Vertical2") * stickSpead * Time.deltaTime;
 
-                    XZangle -= mouse_x_delta * (lookMode ? CameraDisS : CameraDisP) / 10;
-                    Yangle -= mouse_y_delta * (lookMode ? CameraDisS : CameraDisP) / 10;
+                    XZangle -= mouse_x_delta ;
+                    Yangle -= mouse_y_delta ;
 
                     if (Yangle > 90)
                     {
