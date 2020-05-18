23291050563e733d8774dfc526dd143872199ca5
sakuma <syoma.0415@gmail.com>
2020-05-18 11:40:55 +0900

    [チョー聖]

--- AMAZURU/Assets/Sakuma/Script/TitleController.cs
+++ AMAZURU/Assets/Sakuma/Script/TitleController.cs
@@ -5,22 +5,39 @@ using UnityEngine;
 public class TitleController : MonoBehaviour
 {
     [SerializeField, Tooltip("タイトル用のアニメーションスクリプト")] private TitleAnimation titleAnime = null;
-    
 
+    float changeTime = 0;
+    bool changeFlg = false;
     private void Start()
     {
         SoundManager.soundManager.PlayBgm("MusMus-BGM-043", 0.1f, 0.8f,0);
         SoundManager.soundManager.PlayBgm("rain_loop",0.1f, 0.5f,1);
+
+        changeTime = 0;
+        changeFlg = false;
     }
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetButtonDown("Circle")&&!SceneLoadManager .Instance.SceneLoadFlg)
+        if(Input.GetButtonDown("Circle")&& !changeFlg)
         {
+            changeFlg = true;
             SoundManager.soundManager.PlaySe("btn01", 0.5f);
-            titleAnime.AnimationFlag = true;
-            SceneLoadManager.Instance.LoadScene(SceneLoadManager.SceneName .StageSlect ,false );
-            SoundManager.soundManager.StopBgm(1,0);
         }
+
+        if (changeFlg)
+        {
+            changeTime += Time.deltaTime;
+            if (changeTime > 0.5f && !SceneLoadManager.Instance.SceneLoadFlg)
+            {
+                titleAnime.AnimationFlag = true;
+                SceneLoadManager.Instance.LoadScene(SceneLoadManager.SceneName.StageSlect, false);
+                SoundManager.soundManager.StopBgm(1, 0);
+            }
+        }
+
+
+
+
     }
 }
