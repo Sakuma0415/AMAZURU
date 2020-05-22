56ff672d6ce78a1165d5b85a1f868756b291e751
kogemame <banpa1102@gmail.com>
2020-05-22 19:25:39 +0900

    サンプルアセットの追加、サンプルアセット配置用シーン追加

---
+++ AMAZURU/Assets/Uchimura/Sample_Asset/OldTIL/_EnvironmentSEC/Water Shader/_Resources/WATER FOG/WaterFog.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class WaterFog : MonoBehaviour
+{
+	protected MeshRenderer waterFogObj;
+    public Material FogMaterial;
+	 float scaleFactor = 1;
+
+	void OnEnable ()
+	{
+        waterFogObj = gameObject.GetComponent<MeshRenderer>();
+        if (waterFogObj == null)
+			Debug.LogError("Volume Fog Object must have a MeshRenderer Component!");
+		
+		//Note: In forward lightning path, the depth texture is not automatically generated.
+		if (Camera.main.depthTextureMode == DepthTextureMode.None)
+			Camera.main.depthTextureMode = DepthTextureMode.Depth;
+		
+        waterFogObj.material = FogMaterial;
+		
+	}
+
+	void Update ()
+	{
+		float radius = (transform.lossyScale.x + transform.lossyScale.y + transform.lossyScale.z) / 6;
+        Material mat = Application.isPlaying ? waterFogObj.material : waterFogObj.sharedMaterial;
+		if (mat)
+			mat.SetVector ("FogParam", new Vector4(transform.position.x, transform.position.y, transform.position.z, radius * scaleFactor));
+	}
+}
