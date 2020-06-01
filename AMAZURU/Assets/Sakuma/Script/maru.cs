a56264d21f3dc2693be87f17e7ff74d173f9879c
sakuma <syoma.0415@gmail.com>
2020-06-01 18:43:16 +0900

    崖更新

--- AMAZURU/Assets/Sakuma/Script/maru.cs
+++ AMAZURU/Assets/Sakuma/Script/maru.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class maru : MonoBehaviour
 {
+    Vector3 pos;
+    float time;
     // Start is called before the first frame update
     void Start()
     {
-        
+        pos = transform.position;
+        time = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.eulerAngles = new Vector3 (0, Camera.main.transform.eulerAngles.y,0);
+        time += Time.deltaTime;
+        transform.position = pos + new Vector3(0,Mathf.Sin ((time*360)*Mathf.Deg2Rad)*0.15f,0);
     }
 }
