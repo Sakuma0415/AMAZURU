475f3fa6e36c46ac1181627bd40c983b7fdb6077
sakuma <syoma.0415@gmail.com>
2020-06-12 11:09:28 +0900

    [Update] コントローラーテスト

---
+++ AMAZURU/Assets/Sakuma/Script/ControllerInput.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerInput : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(Input.GetJoystickNames().Length);
+        string contName = "";
+        for(int i=0;i< Input.GetJoystickNames().Length; i++)
+        {
+            if (Input.GetJoystickNames()[i] != "")
+            {
+                contName = Input.GetJoystickNames()[i];
+                break;
+            }
+        }
+
+        Debug.Log("接続しているコントローラーは "+contName+" です。");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
