d42ca3f427d3128ee07c1f9396eb4f01cfb0411a
sakuma <syoma.0415@gmail.com>
2020-07-10 16:14:30 +0900

    フィルター修正

--- AMAZURU/Assets/Sakuma/Script/Test/VewPos.cs
+++ AMAZURU/Assets/Sakuma/Script/Test/VewPos.cs
@@ -16,7 +16,7 @@ public class VewPos : MonoBehaviour
     
     void Update()
     {
-        Debug.Log( RectTransformUtility.WorldToScreenPoint(Camera.main, PlayerTransform.position));
+        //Debug.Log( RectTransformUtility.WorldToScreenPoint(Camera.main, PlayerTransform.position));
         Vector2  centerPos =( RectTransformUtility.WorldToScreenPoint(Camera.main, PlayerTransform.position) + RectTransformUtility.WorldToScreenPoint(Camera.main, PlayerTransform.position + new Vector3(0, headPos, 0)))/ 2;
         float r = Vector2.Distance(RectTransformUtility.WorldToScreenPoint(Camera.main, PlayerTransform.position), RectTransformUtility.WorldToScreenPoint(Camera.main, PlayerTransform.position + new Vector3(0, headPos, 0)))+0.2f;
         Vector4 Ass = new Vector4(centerPos.x/1920, centerPos.y/1080,0,0);
