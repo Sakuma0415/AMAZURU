df3b96f8f52a479221c778e691012ac0223cf683
Kazunori Hara <rize.game.0816@gmail.com>
2020-08-05 17:31:31 +0900

    [Update]Edditor上でプレイしているときのみ、ステージセレクトでTキーを押すことでテストステージに行けるように変更

---
+++ AMAZURU/Assets/Hara/Scripts/StageObject/TestStage.cs
@@ -0,0 +1,24 @@
+﻿#if UNITY_EDITOR
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestStage : MonoBehaviour
+{
+    [SerializeField] StageData testStageData = null;
+    // Update is called once per frame
+    void Update()
+    {
+        if(testStageData == null) { return; }
+
+        if (Input.GetKeyDown(KeyCode.T) && SceneLoadManager.Instance.SceneLoadFlg == false)
+        {
+            StageMake.LoadStageData = testStageData;
+            SoundManager.soundManager.StopBgm(0.5f, 1);
+            SoundManager.soundManager.StopBgm(0.5f, 0);
+            SoundManager.soundManager.PlaySe("btn01", 0.3f);
+            SceneLoadManager.Instance.LoadScene(SceneLoadManager.SceneName.Action);
+        }
+    }
+}
+#endif
