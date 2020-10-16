1880234384f598ea4fdffdf49b4ebd03d0796eec
sakuma <syoma.0415@gmail.com>
2020-10-16 17:36:31 +0900

    回転板

--- AMAZURU/Assets/Sakuma/Script/Action/kaiten/RotationPot.cs
+++ AMAZURU/Assets/Sakuma/Script/Action/kaiten/RotationPot.cs
@@ -6,29 +6,65 @@ public class RotationPot : MonoBehaviour
 {
     [SerializeField]
     float goAngle = 0;
-    [SerializeField]
     Vector3 lotAngle = Vector3.zero;
-    [SerializeField]
     Vector3 WavelotAngle = Vector3.zero;
+
+
+    public enum DirectionRot
+    {
+        N=0,
+        S,
+        E,
+        W,
+    }
+
+    [SerializeField]
+    DirectionRot directionRot;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        switch ((int)directionRot)
+        {
+            case 0:
+                lotAngle = new Vector3(90, 0, 0);
+                WavelotAngle = new Vector3(0, -90, 90);
+                return;
+            case 1:
+                lotAngle = new Vector3(-90, 0, 0);
+                WavelotAngle = new Vector3(0, 90, 90);
+                return;
+            case 2:
+                lotAngle = new Vector3(0, 0, -90);
+                WavelotAngle = new Vector3(0, 0, 90);
+                return;
+            case 3:
+                lotAngle = new Vector3(0, 0, 90);
+                WavelotAngle = new Vector3(0, 180, 90);
+                return;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+
+
+
+
+
+
+
         if (set && ControllerInput.Instance.buttonDown.circle && PlayState.playState.gameMode == PlayState.GameMode.Play)
         {
             Debug.Log(goAngle);
-            PlayState .playState . RotationPotStart(lotAngle, WavelotAngle,goAngle, true);
+            PlayState.playState.RotationPotStart(lotAngle, WavelotAngle, goAngle, true);
 
         }
     }
 
 
-    bool set=false ;
+    public bool set = false;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -41,7 +77,7 @@ public class RotationPot : MonoBehaviour
     {
         if (LayerMask.LayerToName(other.gameObject.layer) == "Player")
         {
-            set = false ;
+            set = false;
         }
     }
 
